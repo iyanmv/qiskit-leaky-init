f3ab0ab84e42ef47620d5b8b1da7727f36a1fe0c
Iyán Méndez Veiga <me@iyanmv.com>
2024-07-08 14:49:38 +0200

    Add decoder

--- src/qiskit_leaky_init/__init__.py
+++ src/qiskit_leaky_init/__init__.py
@@ -1,1 +1,2 @@
+from .decoder import extract_data, recover_data
 from .leaky_init_plugin import LeakyInitPlugin
---
+++ src/qiskit_leaky_init/decoder.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+
+from qiskit.circuit import QuantumCircuit, QuantumRegister, Qubit
+
+
+def numbers_to_data(numbers: list, block_size=128):
+    data = b""
+    for number in numbers[:-1]:
+        data += number.to_bytes(block_size)
+    # Last block is special because we have to remove the padding
+    padding = numbers[-1].to_bytes(block_size)[0]
+    data += numbers[-1].to_bytes(block_size)[padding:]
+    return data
+
+
+def recover_data(qc: QuantumCircuit, block_size=128) -> bytes:
+    if qc.layout is None:
+        target_qubit = qc.qubits[-1]
+    else:
+        mapped_index = qc.layout.input_qubit_mapping[
+            Qubit(QuantumRegister(1, "leak"), 0)
+        ]
+        index_qubit = qc.layout.initial_index_layout()[mapped_index]
+        target_qubit = qc.qubits[index_qubit]
+
+    numbers = []
+    for instruction in qc.data:
+        # Not ancilla qubit
+        if instruction.qubits[0] != target_qubit:
+            continue
+
+        # Reset
+        if instruction.operation.name == "reset":
+            continue
+
+        # Useful gate
+        if instruction.operation.name == "rz":
+            numbers.append(instruction.operation.params[0])
+
+    if not numbers:
+        return b""
+
+    return numbers_to_data(numbers, block_size)
+
+
+def extract_data(data: bytes, path: Path) -> None:
+    raise NotImplementedError
